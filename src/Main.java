cb622aeea0c8d50aa157967d4631e7cf49f5bb4c
Burhozturk <31951806+Burhozturk@users.noreply.github.com>
2018-09-30 12:52:35 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double width=5.3;
+        double height=8.6;
+
+        double area=width*height;
+        double perimeter=2*(width+height);
+
+        System.out.println("Area er: " +area);
+        System.out.println("perimeter er: " +perimeter);
+    }
+}
